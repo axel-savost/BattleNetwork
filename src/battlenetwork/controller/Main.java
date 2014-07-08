0ed1ae0231f549b83842b1428156fc1ea9ac9167
Axel S.Ö <axel_gm4@hotmail.com>
2014-07-08 15:13:42 +0200

    The game is now runnable. Run Main.java.

---
+++ src/battlenetwork/controller/Main.java
@@ -0,0 +1,21 @@
+package battlenetwork.controller;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+import battlenetwork.model.Constants;
+
+
+public class Main {
+	public static void main(String[] args) {
+		try {
+            AppGameContainer container = new AppGameContainer(new BattleNetworkGame("Battle Time!"));
+            container.setDisplayMode(Constants.GAME_WIDTH, Constants.GAME_HEIGHT, false);
+            container.setTargetFrameRate(Constants.FRAMERATE);
+            container.start();
+        } catch (SlickException e) { //TODO Proper exception handling. 
+            e.printStackTrace();
+        }
+	}
+
+}
---
+++ src/battlenetwork/model/Constants.java
@@ -0,0 +1,8 @@
+package battlenetwork.model;
+
+public class Constants {
+	public static final int GAME_WIDTH  = 1280;
+	public static final int GAME_HEIGHT = 720;
+	public static final int FRAMERATE   = 60;
+
+}
