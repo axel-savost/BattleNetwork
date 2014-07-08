50738a8d3f27f4bb541b174f9ef9260ae88f3d67
Axel S.Ö <axel_gm4@hotmail.com>
2014-07-08 17:21:13 +0200

    Added sprites for player characters, enemies and an error sprite when someone lacks a sprite.

--- src/battlenetwork/model/Entity.java
+++ src/battlenetwork/model/Entity.java
@@ -1,5 +1,8 @@
 package battlenetwork.model;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
 public abstract class Entity {
 	private Position position;
 	
@@ -14,5 +17,13 @@ public abstract class Entity {
 	public void setPosition(Position position) {
 		this.position = position;
 	}
+	
+	/**
+	 * Runs once every frame
+	 * @param gc
+	 * @param game
+	 * @param i
+	 */
+	public abstract void update(GameContainer gc, StateBasedGame game, int i);
 
 }
--- src/battlenetwork/model/Panel.java
+++ src/battlenetwork/model/Panel.java
@@ -1,9 +1,22 @@
 package battlenetwork.model;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
 public class Panel extends Entity{
 
 	public Panel(Position p) {
 		super(p);
+	}
+	
+	public enum Type{
+		NORMAL, CRACKED, BROKEN, GRASS, LAVA, SAND, METAL, HOLY, POISON;
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame game, int i) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
