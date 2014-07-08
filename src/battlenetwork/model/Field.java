b666004966fa016595ba72cf00bb66caa0407ef0
Anton Nilsson <svante@antons-crunchbang.(none)>
2014-07-08 17:50:44 +0200

    Created field at startup

--- src/battlenetwork/model/Field.java
+++ src/battlenetwork/model/Field.java
@@ -3,29 +3,27 @@ package battlenetwork.model;
 public class Field {
 	private Panel[][] panelGrid = new Panel[Constants.TILES_H*2][Constants.TILES_V];
 	
-	public Field(){
+	public Field(Position p){
 		
 		//Player panels
 		for (int i=0;i<Constants.TILES_H;i++){
 			for (int j=0;j<Constants.TILES_V;j++){
-				panelGrid[i][j] = new Panel(new Position(Constants.PANEL_H*i,Constants.PANEL_W*j));
+				panelGrid[i][j] = new Panel(new Position(p,Constants.PANEL_W*i,Constants.PANEL_H*j));
 			}
 		}
 		//Enemy panels
 		for (int i=0;i<Constants.TILES_H;i++){
 			for (int j=0;j<Constants.TILES_V;j++){
-				panelGrid[i+3][j] = new Panel(new Position(Constants.PANEL_H*(i+3),Constants.PANEL_W*j));
+				panelGrid[i+3][j] = new Panel(new Position(p,Constants.PANEL_W*(i+3),Constants.PANEL_H*j));
 			}
 		}
 	}
 	
 	public int getWidth(){
-		System.out.println("W: " + panelGrid.length);
 		return panelGrid.length;
 	}
 	
 	public int getHeigth(){
-		System.out.println("H: " + panelGrid[0].length);
 		return panelGrid[0].length;
 	}
 	
