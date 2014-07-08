7fd2e86b5b0fc00be11011083f4ce022ee8f59f9
Axel S.Ö <axel_gm4@hotmail.com>
2014-07-08 13:38:54 +0200

    Initial commit: Adding packages and master classes.

---
+++ src/battlenetwork/controller/BattleController.java
@@ -0,0 +1,13 @@
+package battlenetwork.controller;
+
+public class BattleController {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
